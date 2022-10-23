6dc46fed5d18dc92ece8a92bab029c1e647d0453
StephenOdhiambo <stephen.odhiambo29@gmail.com>
2022-10-23 11:50:19 -0700

    print all alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio>
+/*
+*main: start of the program
+*always returns : 0
+**/
+int main(void)
+{
+	int ch='z';
+	int CH='Z';
+
+	while(ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while(CH <='Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
