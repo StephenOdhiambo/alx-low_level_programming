3b2d84e780a71aac98fb9a1872ee19736e1c03cf
StephenOdhiambo <stephen.odhiambo29@gmail.com>
2022-10-23 11:35:47 -0700

    print alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,25 +7,12 @@
 
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
-
-	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
-		{
-		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
-			{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repeatition*/
-				{
-				putchar(tens);
-				putchar(ones);
-					if (!(ones == '9' && tens == '8'))/*addes comma and space*/
-						{
-						putchar(',');
-						putchar(' ');
-						}
-				}
-			}
-		}
+	int ch = 'a';
+	while (ch <= '2')
+	{
+		putchar(ch);
+		ch++;
+	}
 	putchar('\n');
 	return (0);
 }
