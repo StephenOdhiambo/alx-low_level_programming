2f54360519447d9ca82307769a3b0ce1b19f823a
StephenOdhiambo <stephen.odhiambo29@gmail.com>
2023-09-08 19:16:53 -0700

    Print alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,23 +7,10 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+	char a;
 
-	for (i = 10; i <= 19; i++)
-	{
-		for (j = 10; j <= 19; j++)
-		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-			if (i != 19 ||  j != 19)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-
-		}
-	}
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
 	putchar('\n');
 	return (0);
-}
+}}
