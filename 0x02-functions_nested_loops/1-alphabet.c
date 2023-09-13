8cd6053ea6897fc705d27fa6b7d385762526b50d
StephenOdhiambo <stephen.odhiambo29@gmail.com>
2023-09-13 14:27:52 -0700

    Prints a to z in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ * Print alphabet in small letters
+ *
+ * Return 0 value
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i=< 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
+
