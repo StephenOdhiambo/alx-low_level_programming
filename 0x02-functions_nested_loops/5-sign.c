b25c29bfeea4a061e315f25588db6078690e3dd0
StephenOdhiambo <stephen.odhiambo29@gmail.com>
2023-09-14 22:51:13 -0700

    Checks whether number is greater than zero equal or less and returns a value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_sign - Determins if the argument is greater than 
- * equal to or less than zero 
+ * print_sign - Determins if the argument is greater than
+ * equal to or less than zero
  *
- * @n - is an integer
+ * @n: is an integer
  *
  * Return: 1 if n is greater than 0, zero when n is 0
  * and -1 if n is less than 0
@@ -14,17 +14,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		return(-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar(48);
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
