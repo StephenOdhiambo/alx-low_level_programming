381f15c106a79c2999d2a9b908aa0f6e19c30454
StephenOdhiambo <stephen.odhiambo29@gmail.com>
2023-09-15 00:17:29 -0700

    Prints natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
-
+/**
+ * print_to_98 - accepts numbers to be printed
+ *
+ * @n: numbers to be printed
+ *
+ */
 void print_to_98(int n)
 {
 	int i;
